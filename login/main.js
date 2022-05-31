a4ada179261d35dd905de63397a136ceeed99b32
KimSungJin <86825816+aggie4@users.noreply.github.com>
2022-05-31 17:19:41 +0900

    Add files via upload

---
+++ login/main.js
@@ -0,0 +1,14 @@
+id = prompt('아이디를 입력하시오');
+if (id == 'sungjin') {
+    alert('아이디가 일치합니다.');
+} else {
+    alert('아이디가 일치하지 않습니다.');
+}
+password = prompt('비밀번호를 입력하시오');
+if (password == 123456) {
+    alert('비밀번호가 일치합니다.');
+    alert('환영합니다');
+} else {
+    alert('비밀번호가 일치하지 않습니다.');
+    alert('로그인을 실패했습니다.');
+}
